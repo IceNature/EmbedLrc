9885617372012a24c36eb0c92177ddf16d4a2d50
IceNature <icenaturetemp@sina.com>
2015-11-12 20:10:09 +0800

    Fix the error when combining path.

--- EmbedLrc.py
+++ EmbedLrc.py
@@ -15,7 +15,7 @@ supportLrcTypes = ['.lrc']
 def ScanFiles(directory):
     filelist = []
     allObjects = [
-        directory + os.sep + filename for filename in os.listdir(directory)]
+        os.path.join(directory.strip(), filename) for filename in os.listdir(directory)]
     for m in allObjects:
         if os.path.isfile(m):
             filelist.append(m)
